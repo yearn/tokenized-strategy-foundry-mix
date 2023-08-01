956c959feeda2b6dec45870c5cb91a0092d1e5bd
Poolpi Tako <poolpitako@protonmail.com>
2023-07-31 21:31:16 -0400

    fix: typos

--- src/periphery/StrategyAprOracle.sol
+++ src/periphery/StrategyAprOracle.sol
@@ -8,28 +8,28 @@ contract StrategyAprOracle is AprOracleBase {
 
     /**
      * @notice Will return the expected Apr of a strategy post a debt change.
-     * @dev _delta is a signed integer so that it can also repersent a debt
+     * @dev _delta is a signed integer so that it can also represent a debt
      * decrease.
      *
      * This should return the annual expected return at the current timestamp
-     * repersented as 1e18.
+     * represented as 1e18.
      *
      *      ie. 10% == 1e17
      *
      * _delta will be == 0 to get the current apr.
      *
      * This will potentially be called during non-view functions so gas
-     * effeciency should be taken into account.
+     * efficiency should be taken into account.
      *
      * @param _strategy The token to get the apr for.
      * @param _delta The difference in debt.
-     * @return . The expected apr for the strategy repersented as 1e18.
+     * @return . The expected apr for the strategy represented as 1e18.
      */
     function aprAfterDebtChange(
         address _strategy,
         int256 _delta
     ) external view override returns (uint256) {
-        // TODO: Implement any neccesary logic to return the most accurate
+        // TODO: Implement any necessary logic to return the most accurate
         //      APR estimation for the strategy.
         return 1e17;
     }
