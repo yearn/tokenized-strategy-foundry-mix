b2671ba367408b920eac33c1ba8b58c1327330de
dudesahn <23222916+dudesahn@users.noreply.github.com>
2024-07-25 16:52:39 -0400

    chore: bump version (#34)
    * chore: bump version
    
    bump to 0.8.23
    
    * chore: update submodules
    
    * fix: greater than 18
    
    * fix: redo requirements
    
    * feat: one more bump
    
    * fix: use console2
    
    ---------
    
    Co-authored-by: Schlag <89420541+Schlagonia@users.noreply.github.com>

--- src/test/Shutdown.t.sol
+++ src/test/Shutdown.t.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.18;
 
-import "forge-std/console.sol";
+import "forge-std/console2.sol";
 import {Setup, ERC20, IStrategyInterface} from "./utils/Setup.sol";
 
 contract ShutdownTest is Setup {
