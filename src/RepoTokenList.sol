916b05d54f782342f97db6d9fddefe84380227e9
Andrew Zhou <andrew@termfinance.io>
2024-09-09 18:27:56 -0700

    miscellaneous audit fixes

--- src/RepoTokenList.sol
+++ src/RepoTokenList.sol
@@ -324,7 +324,7 @@ library RepoTokenList {
         (redemptionTimestamp, purchaseToken, , collateralManager) = repoToken.config();
 
         // Validate purchase token
-        if (purchaseToken != address(asset)) {
+        if (purchaseToken != asset) {
             revert InvalidRepoToken(address(repoToken));
         }
 
