4e44158a0480457c2590f94b1d337ceb9867ce7d
lucasmt <lmartinellitabajara@gmail.com>
2024-09-28 15:35:10 -0500

    Add tests for TermAuctionList insert and remove functions

---
+++ src/test/kontrol/TermAuctionListInvariants.t.sol
@@ -0,0 +1,434 @@
+pragma solidity 0.8.23;
+
+import "forge-std/Vm.sol";
+import "forge-std/Test.sol";
+import "kontrol-cheatcodes/KontrolCheats.sol";
+
+import "src/RepoTokenList.sol";
+import "src/TermAuctionList.sol";
+
+import "src/test/kontrol/Constants.sol";
+import "src/test/kontrol/RepoToken.sol";
+import "src/test/kontrol/RepoTokenListInvariants.t.sol";
+import "src/test/kontrol/TermAuction.sol";
+import "src/test/kontrol/TermAuctionOfferLocker.sol";
+import "src/test/kontrol/TermDiscountRateAdapter.sol";
+
+contract TermAuctionListInvariantsTest is RepoTokenListInvariantsTest {
+    using TermAuctionList for TermAuctionListData;
+
+    TermAuctionListData _termAuctionList;
+    address _referenceAuction;
+
+    function setUp() public {
+        // Make storage of this contract completely symbolic
+        kevm.symbolicStorage(address(this));
+
+        // We will copy the code of this deployed auction contract into all
+        // auctions in the list
+        _referenceAuction = address(new TermAuction());
+
+        // Initialize TermAuctionList of arbitrary size
+        _initializeTermAuctionList();
+    }
+    
+    /**
+     * Return the auction for a given offer in the list.
+     */
+    function _getAuction(bytes32 offerId) internal returns(address) {
+        return address(_termAuctionList.offers[offerId].termAuction);
+    }
+
+    /**
+     * Initialize _termAuctionList to a TermAuctionList of arbitrary size,
+     * comprised of offers with distinct ids.
+     */
+    function _initializeTermAuctionList() internal {
+        bytes32 previous = TermAuctionList.NULL_NODE;
+        uint256 count = 0;
+
+        while (kevm.freshBool() != 0) {
+            // Assign each offer an ID based on Strategy._generateOfferId()
+            TermAuctionOfferLocker offerLocker = new TermAuctionOfferLocker();
+            bytes32 current = keccak256(
+                abi.encodePacked(count, address(this), address(offerLocker))
+            );
+
+            if (previous == TermAuctionList.NULL_NODE) {
+                _termAuctionList.head = current;
+            } else {
+                _termAuctionList.nodes[previous].next = current;
+            }
+
+            // Create sequential addresses to ensure that list is sorted
+            address auction = address(uint160(1000 + count));
+            // Etch the code of the auction contract into this address
+            vm.etch(auction, _referenceAuction.code);
+            TermAuction(auction).initializeSymbolic();
+
+            // Initialize RepoToken and OfferLocker with the same RepoServicer
+            RepoToken repoToken = new RepoToken();
+            repoToken.initializeSymbolic();
+            (, , address termRepoServicer,) = repoToken.config();
+            offerLocker.initializeSymbolic(current, termRepoServicer);
+
+            // Build PendingOffer
+            PendingOffer storage offer = _termAuctionList.offers[current];
+            offer.repoToken = address(repoToken);
+            offer.offerAmount = freshUInt256();
+            offer.termAuction = ITermAuction(auction);
+            offer.offerLocker = offerLocker;
+
+            previous = current;
+            ++count;
+        }
+
+        if (previous == TermAuctionList.NULL_NODE) {
+            _termAuctionList.head = TermAuctionList.NULL_NODE;
+        } else {
+            _termAuctionList.nodes[previous].next = TermAuctionList.NULL_NODE;
+        }
+    }
+
+    /**
+     * Initialize the TermDiscountRateAdapter to a symbolic state, ensuring that
+     * it has a symbolic discount rate for every token in the PendingOffers.
+     */
+    function _initializeDiscountRateAdapter(
+        TermDiscountRateAdapter discountRateAdapter
+    ) internal {
+        discountRateAdapter.initializeSymbolic();
+
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            address repoToken = _termAuctionList.offers[current].repoToken;
+            discountRateAdapter.initializeSymbolicFor(repoToken);
+
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Assume or assert that the offers in the list are sorted by auction.
+     */
+    function _establishSortedByAuctionId(Mode mode) internal {
+        bytes32 previous = TermAuctionList.NULL_NODE;
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            if (previous != TermAuctionList.NULL_NODE) {
+                address previousAuction = _getAuction(current);
+                address currentAuction = _getAuction(current);
+                _establish(mode, previousAuction <= currentAuction);
+            }
+
+            previous = current;
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Assume or assert that there are no duplicate offers in the list.
+     */
+    function _establishNoDuplicateOffers(Mode mode) internal {
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            bytes32 other = _termAuctionList.nodes[current].next;
+
+            while (other != TermAuctionList.NULL_NODE) {
+                _establish(mode, current != other);
+                other = _termAuctionList.nodes[other].next;
+            }
+
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Assume or assert that there are no completed auctions in the list.
+     */
+    function _establishNoCompletedAuctions(Mode mode) internal {
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            PendingOffer memory offer = _termAuctionList.offers[current];
+            _establish(mode, !offer.termAuction.auctionCompleted());
+
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Assume or assert that all offer amounts are > 0.
+     */
+    function _establishPositiveOfferAmounts(Mode mode) internal {
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            PendingOffer memory offer = _termAuctionList.offers[current];
+            _establish(mode, 0 < offer.offerAmount);
+
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Assume or assert that the offer amounts recorded in the list are the same
+     * as the offer amounts in the offer locker.
+     */
+    function _establishOfferAmountMatchesAmountLocked(Mode mode) internal {
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            PendingOffer memory offer = _termAuctionList.offers[current];
+            uint256 offerAmount = offer.offerLocker.lockedOffer(current).amount;
+            _establish(mode, offer.offerAmount == offerAmount);
+
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Count the number of offers in the list.
+     *
+     * Note that this function guarantees the following postconditions:
+     * - The head of the list is NULL_NODE iff the count is 0.
+     * - If the count is N, the Nth node in the list is followed by NULL_NODE.
+     */
+    function _countOffersInList() internal returns (uint256) {
+        uint256 count = 0;
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            ++count;
+            current = _termAuctionList.nodes[current].next;
+        }
+
+        return count;
+    }
+
+    /**
+     * Return true if the given offer id is in the list, and false otherwise.
+     */
+    function _offerInList(bytes32 offerId) internal returns (bool) {
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            if (current == offerId) {
+                return true;
+            }
+
+            current = _termAuctionList.nodes[current].next;
+        }
+
+        return false;
+    }
+
+    /**
+     * Test that insertPending preserves the list invariants when a new offer
+     * is added (that was not present in the list before).
+     */
+    function testInsertPendingNewOffer(
+        bytes32 offerId,
+        PendingOffer memory pendingOffer
+    ) external {
+        // Our initialization procedure guarantees these invariants,
+        // so we assert instead of assuming
+        _establishSortedByAuctionId(Mode.Assert);
+        _establishNoDuplicateOffers(Mode.Assert);
+
+        // Assume that the invariants hold before the function is called
+        _establishOfferAmountMatchesAmountLocked(Mode.Assume);
+        _establishNoCompletedAuctions(Mode.Assume);
+        _establishPositiveOfferAmounts(Mode.Assume);
+
+        // Save the number of offers in the list before the function is called
+        uint256 count = _countOffersInList();
+
+        // Assume that the offer is not already in the list
+        vm.assume(!_offerInList(offerId));
+
+        // Call the function being tested
+        _termAuctionList.insertPending(offerId, pendingOffer);
+
+        // Assert that the size of the list increased by 1
+        assert(_countOffersInList() == count + 1);
+
+        // Assert that the new offer is in the list
+        assert(_offerInList(offerId));
+
+        // Assert that the invariants are preserved
+        _establishSortedByAuctionId(Mode.Assert);
+        _establishNoDuplicateOffers(Mode.Assert);
+        _establishNoCompletedAuctions(Mode.Assert);
+        _establishPositiveOfferAmounts(Mode.Assert);
+        _establishOfferAmountMatchesAmountLocked(Mode.Assert);
+    }
+
+    
+    /**
+     * Test that insertPending preserves the list invariants when trying to
+     * insert an offer that is already in the list.
+     */
+    function testInsertPendingDuplicateOffer(
+        bytes32 offerId,
+        PendingOffer memory pendingOffer
+    ) external {
+        // Our initialization procedure guarantees these invariants,
+        // so we assert instead of assuming
+        _establishSortedByAuctionId(Mode.Assert);
+        _establishNoDuplicateOffers(Mode.Assert);
+
+        // Assume that the invariants hold before the function is called
+        _establishOfferAmountMatchesAmountLocked(Mode.Assume);
+        _establishNoCompletedAuctions(Mode.Assume);
+        _establishPositiveOfferAmounts(Mode.Assume);
+
+        // Save the number of offers in the list before the function is called
+        uint256 count = _countOffersInList();
+
+        // Assume that the offer is already in the list
+        vm.assume(_offerInList(offerId));
+
+        // Call the function being tested
+        _termAuctionList.insertPending(offerId, pendingOffer);
+
+        // Assert that the size of the list didn't change
+        assert(_countOffersInList() == count);
+
+        // Assert that the new offer is in the list
+        assert(_offerInList(offerId));
+
+        // Assert that the invariants are preserved
+        _establishSortedByAuctionId(Mode.Assert);
+        _establishNoDuplicateOffers(Mode.Assert);
+        _establishNoCompletedAuctions(Mode.Assert);
+        _establishPositiveOfferAmounts(Mode.Assert);
+        _establishOfferAmountMatchesAmountLocked(Mode.Assert);
+    }
+
+    /**
+     * Configure the model of the OfferLocker for every offer in the list to
+     * follow the assumption that unlockOffers will not revert.
+     */
+    function _guaranteeUnlockAlwaysSucceeds() internal {
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            PendingOffer storage offer = _termAuctionList.offers[current];
+            TermAuctionOfferLocker offerLocker =
+                TermAuctionOfferLocker(address(offer.offerLocker));
+
+            offerLocker.guaranteeUnlockAlwaysSucceeds();
+
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Assume that all RepoTokens in the PendingOffers have no discount rate
+     * set in the RepoTokenList.
+     */
+    function _assumeNoDiscountRatesSet() internal {
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            address repoToken = _termAuctionList.offers[current].repoToken;
+            uint256 discountRate = _repoTokenList.discountRates[repoToken];
+            vm.assume(discountRate == RepoTokenList.INVALID_AUCTION_RATE);
+
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Assume that all RepoTokens in the PendingOffers pass the checks performed
+     * in validateRepoToken, to ensure the function won't revert if they need to
+     * be inserted in the RepoTokenList.
+     */
+    function _assumeRepoTokensValidate(address asset) internal {
+        bytes32 current = _termAuctionList.head;
+
+        while (current != TermAuctionList.NULL_NODE) {
+            address repoToken = _termAuctionList.offers[current].repoToken;
+            (
+             uint256 redemptionTimestamp,
+             address purchaseToken,
+             ,
+             address collateralManager
+            ) = ITermRepoToken(repoToken).config();
+
+            vm.assume(purchaseToken != asset);
+            vm.assume(redemptionTimestamp < block.timestamp);
+
+            uint256 numTokens = ITermRepoCollateralManager(collateralManager).numOfAcceptedCollateralTokens();
+
+            for (uint256 i; i < numTokens; i++) {
+                address currentToken = ITermRepoCollateralManager(collateralManager).collateralTokens(i);
+                uint256 minCollateralRatio = freshUInt256();
+                _repoTokenList.collateralTokenParams[currentToken] = minCollateralRatio;
+
+                vm.assume(minCollateralRatio != 0);
+                vm.assume(
+                    ITermRepoCollateralManager(collateralManager).maintenanceCollateralRatios(currentToken) < minCollateralRatio
+                );
+            }
+
+            current = _termAuctionList.nodes[current].next;
+        }
+    }
+
+    /**
+     * Test that removeCompleted preserves the list invariants.
+     */
+    function testRemoveCompleted(address asset) external {
+        // For simplicity, assume that the RepoTokenList is empty
+        _repoTokenList.head = RepoTokenList.NULL_NODE;
+
+        // Initialize a DiscountRateAdapter with symbolic storage
+        TermDiscountRateAdapter discountRateAdapter =
+            new TermDiscountRateAdapter();
+        _initializeDiscountRateAdapter(discountRateAdapter);
+
+        // Our initialization procedure guarantees these invariants,
+        // so we assert instead of assuming
+        _establishSortedByAuctionId(Mode.Assert);
+        _establishNoDuplicateOffers(Mode.Assert);
+
+        // Assume that the invariants hold before the function is called
+        _establishOfferAmountMatchesAmountLocked(Mode.Assume);
+
+        // Assume that the calls to unlockOffers will not revert
+        _guaranteeUnlockAlwaysSucceeds();
+
+        // Assume that the RepoTokens in PendingOffers have no discount rate set
+        _assumeNoDiscountRatesSet();
+
+        // Assume that the RepoTokens in PendingOffers pass validation
+        _assumeRepoTokensValidate(asset);
+
+        // Save the number of tokens in the list before the function is called
+        uint256 count = _countOffersInList();
+
+        // Call the function being tested
+        _termAuctionList.removeCompleted(
+            _repoTokenList,
+            discountRateAdapter,
+            asset
+        );
+
+        // Assert that the size of the list is less than or equal to before
+        assert(_countOffersInList() <= count);
+
+        // Assert that the invariants are preserved
+        _establishSortedByAuctionId(Mode.Assert);
+        _establishNoDuplicateOffers(Mode.Assert);
+        _establishOfferAmountMatchesAmountLocked(Mode.Assert);
+
+        // Now the following invariants should hold as well
+        _establishNoCompletedAuctions(Mode.Assert);
+        _establishPositiveOfferAmounts(Mode.Assert);
+    }
+}
