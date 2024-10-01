61f504c2b6ce8877e0c971ac010fc9d20f063b19
lucasmt <lmartinellitabajara@gmail.com>
2024-09-30 20:43:05 -0500

    Update TermRepoCollateralManager to use a concrete number of collateral tokens

--- src/test/kontrol/TermRepoCollateralManager.sol
+++ src/test/kontrol/TermRepoCollateralManager.sol
@@ -7,17 +7,21 @@ import "src/interfaces/term/ITermRepoCollateralManager.sol";
 
 contract TermRepoCollateralManager is ITermRepoCollateralManager, Test, KontrolCheats {
     mapping(address => uint256) _maintenanceCollateralRatios;
-    uint8 _numOfAcceptedCollateralTokens;
     address[] _collateralTokens;
 
     function initializeSymbolic() public {
         kevm.symbolicStorage(address(this));
 
-        _numOfAcceptedCollateralTokens = freshUInt8();
+        // For simplicity, choose an arbitrary number of collateral tokens
+        vm.assume(_collateralTokens.length == 3);
 
-        for (uint256 i = 0; i < _numOfAcceptedCollateralTokens; ++i) {
-            address currentToken = kevm.freshAddress();
-            _collateralTokens.push(currentToken);
+        for (uint256 i = 0; i < _collateralTokens.length; ++i) {
+            // Generate an arbitrary concrete address for each token
+            address currentToken = address(
+                uint160(uint256(keccak256(abi.encodePacked("collateral", i))))
+            );
+
+            _collateralTokens[i] = currentToken;
             _maintenanceCollateralRatios[currentToken] = freshUInt256();
         }
     }
@@ -29,7 +33,7 @@ contract TermRepoCollateralManager is ITermRepoCollateralManager, Test, KontrolC
     }
 
     function numOfAcceptedCollateralTokens() external view returns (uint8) {
-        return _numOfAcceptedCollateralTokens;
+        return uint8(_collateralTokens.length);
     }
 
     function collateralTokens(uint256 index) external view returns (address) {
