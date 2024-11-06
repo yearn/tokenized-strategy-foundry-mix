5e21dba2f0e6bf8b687f5d6f589ce94201fc6bf7
aazhou1 <andrew@termfinance.io>
2024-11-06 12:20:49 -0800

    strategy accept management script for sepolia

---
+++ script/StrategyManagement.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "@tokenized-strategy/interfaces/ITokenizedStrategy.sol";
+
+contract StrategyManagement is Script {
+    function run() external {
+        address strategyManagement = vm.envAddress("STRATEGY_MANAGEMENT_ADDRESS");
+        address strategy = vm.envAddress("STRATEGY");
+
+        // Set up the RPC URL (optional if you're using the default foundry config)
+        string memory rpcUrl = vm.envString("RPC_URL");
+        
+        vm.startBroadcast(strategyManagement);
+        ITokenizedStrategy(strategy).acceptManagement();
+        console.log("accepted management");
+        vm.stopBroadcast();
+
+
+    }
+}
