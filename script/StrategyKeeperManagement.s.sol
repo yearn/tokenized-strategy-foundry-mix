23ba79029a77de09edcde676804f1ebbbf55a14d
aazhou1 <andrew@termfinance.io>
2024-11-11 17:10:30 -0500

    set strategy keeper

---
+++ script/StrategyKeeperManagement.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "@tokenized-strategy/interfaces/ITokenizedStrategy.sol";
+
+contract StrategyKeeperManagement is Script {
+    function run() external {
+        uint256 keeperAddress = vm.envUint("KEEPER_ADDRESS");
+        address strategy = vm.envAddress("STRATEGY");
+
+        // Set up the RPC URL (optional if you're using the default foundry config)
+        string memory rpcUrl = vm.envString("RPC_URL");
+        
+        vm.startBroadcast(strategyManagement);
+        ITokenizedStrategy(strategy).setKeeper(keeperAddress);
+        console.log("accepted management");
+        vm.stopBroadcast();
+    }
+}
