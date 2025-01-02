1ab027afc8f2ca5dd48f538171ced4544fbd25bc
aazhou1 <7308872+aazhou1@users.noreply.github.com>
2025-01-02 18:23:47 +0000

    [stylebot] Fixes for code style

--- src/helper/TermVaultsKeeper.sol
+++ src/helper/TermVaultsKeeper.sol
@@ -16,86 +16,146 @@ interface IVault {
 }
 
 interface ICommonReportTrigger {
-    function defaultStrategyReportTrigger(address _strategy) external view returns (bool, bytes memory);
-    function defaultVaultReportTrigger(address _vault, address _strategy) external view returns (bool, bytes memory);
+    function defaultStrategyReportTrigger(
+        address _strategy
+    ) external view returns (bool, bytes memory);
+    function defaultVaultReportTrigger(
+        address _vault,
+        address _strategy
+    ) external view returns (bool, bytes memory);
 }
 
-contract TermVaultsKeeper is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
+contract TermVaultsKeeper is
+    Initializable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable
+{
     bytes32 public constant KEEPER_ROLE = keccak256("KEEPER_ROLE");
-    address public constant COMMON_REPORT_TRIGGER_ADDRESS = 0xA045D4dAeA28BA7Bfe234c96eAa03daFae85A147;
+    address public constant COMMON_REPORT_TRIGGER_ADDRESS =
+        0xA045D4dAeA28BA7Bfe234c96eAa03daFae85A147;
 
-     /// @custom:oz-upgrades-unsafe-allow constructor
+    /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address devops, address initialKeeper) public initializer {
+    function initialize(
+        address devops,
+        address initialKeeper
+    ) public initializer {
         __AccessControl_init();
         _setupRole(DEFAULT_ADMIN_ROLE, devops);
         _setupRole(KEEPER_ROLE, initialKeeper);
     }
 
-    function reports(address[] calldata strategies, address vault, address[] calldata vaultStrategies) external onlyRole(KEEPER_ROLE) {
+    function reports(
+        address[] calldata strategies,
+        address vault,
+        address[] calldata vaultStrategies
+    ) external onlyRole(KEEPER_ROLE) {
         _callStrategyReports(strategies);
         _processReports(vault, vaultStrategies);
     }
 
-    function _callStrategyReports(address[] calldata strategies) internal  {
-        ICommonReportTrigger commonReportTrigger = ICommonReportTrigger(COMMON_REPORT_TRIGGER_ADDRESS);
+    function _callStrategyReports(address[] calldata strategies) internal {
+        ICommonReportTrigger commonReportTrigger = ICommonReportTrigger(
+            COMMON_REPORT_TRIGGER_ADDRESS
+        );
         for (uint256 i = 0; i < strategies.length; i++) {
-            (bool shouldReport, ) = commonReportTrigger.defaultStrategyReportTrigger(strategies[i]);
+            (bool shouldReport, ) = commonReportTrigger
+                .defaultStrategyReportTrigger(strategies[i]);
             if (shouldReport) {
                 IStrategy(strategies[i]).report();
             }
         }
     }
 
-    function _processReports(address vault, address[] calldata strategies) internal {
-        ICommonReportTrigger commonReportTrigger = ICommonReportTrigger(COMMON_REPORT_TRIGGER_ADDRESS);
+    function _processReports(
+        address vault,
+        address[] calldata strategies
+    ) internal {
+        ICommonReportTrigger commonReportTrigger = ICommonReportTrigger(
+            COMMON_REPORT_TRIGGER_ADDRESS
+        );
         for (uint256 i = 0; i < strategies.length; i++) {
-            (bool shouldReport, ) = commonReportTrigger.defaultVaultReportTrigger(vault, strategies[i]);
+            (bool shouldReport, ) = commonReportTrigger
+                .defaultVaultReportTrigger(vault, strategies[i]);
             if (shouldReport) {
                 IVault(vault).process_report(strategies[i]);
             }
         }
     }
 
-    function reportsNoTriggerCheck(address[] calldata strategies, address vault, address[] calldata vaultStrategies) external onlyRole(KEEPER_ROLE) {
+    function reportsNoTriggerCheck(
+        address[] calldata strategies,
+        address vault,
+        address[] calldata vaultStrategies
+    ) external onlyRole(KEEPER_ROLE) {
         _callStrategyReportsNoTriggerCheck(strategies);
         _processReportsNoTriggerCheck(vault, vaultStrategies);
     }
 
-    function _callStrategyReportsNoTriggerCheck(address[] calldata strategies) internal  {
+    function _callStrategyReportsNoTriggerCheck(
+        address[] calldata strategies
+    ) internal {
         for (uint256 i = 0; i < strategies.length; i++) {
             IStrategy(strategies[i]).report();
         }
     }
 
-    function _processReportsNoTriggerCheck(address vault, address[] calldata strategies) internal {
+    function _processReportsNoTriggerCheck(
+        address vault,
+        address[] calldata strategies
+    ) internal {
         for (uint256 i = 0; i < strategies.length; i++) {
             IVault(vault).process_report(strategies[i]);
         }
     }
 
-    function rebalanceVault(address vault, address[] calldata withdrawStrategies, uint256[] calldata withdrawTargetAmounts, address[] calldata depositStrategies, uint256[] calldata depositTargetAmounts) external onlyRole(KEEPER_ROLE) {
+    function rebalanceVault(
+        address vault,
+        address[] calldata withdrawStrategies,
+        uint256[] calldata withdrawTargetAmounts,
+        address[] calldata depositStrategies,
+        uint256[] calldata depositTargetAmounts
+    ) external onlyRole(KEEPER_ROLE) {
         require(vault != address(0), "0 address vault");
         _withdraw(vault, withdrawStrategies, withdrawTargetAmounts);
         _deposit(vault, depositStrategies, depositTargetAmounts);
     }
 
-    function _withdraw(address vault, address[] calldata withdrawStrategies, uint256[] calldata withdrawTargetAmounts) internal {
+    function _withdraw(
+        address vault,
+        address[] calldata withdrawStrategies,
+        uint256[] calldata withdrawTargetAmounts
+    ) internal {
         for (uint256 i = 0; i < withdrawStrategies.length; i++) {
-            IVault(vault).updateDebt(withdrawStrategies[i], withdrawTargetAmounts[i]);
+            IVault(vault).updateDebt(
+                withdrawStrategies[i],
+                withdrawTargetAmounts[i]
+            );
         }
     }
 
-    function _deposit(address vault, address[] calldata depositStrategies, uint256[] calldata depositTargetAmounts) internal {
+    function _deposit(
+        address vault,
+        address[] calldata depositStrategies,
+        uint256[] calldata depositTargetAmounts
+    ) internal {
         for (uint256 i = 0; i < depositStrategies.length - 1; i++) {
-            IVault(vault).updateDebt(depositStrategies[i], depositTargetAmounts[i]);
+            IVault(vault).updateDebt(
+                depositStrategies[i],
+                depositTargetAmounts[i]
+            );
         }
         uint256 vaultIdle = IVault(vault).totalIdle();
-        IVault(vault).updateDebt(depositStrategies[depositStrategies.length - 1], vaultIdle);   
+        IVault(vault).updateDebt(
+            depositStrategies[depositStrategies.length - 1],
+            vaultIdle
+        );
     }
 
-    function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+    function _authorizeUpgrade(
+        address
+    ) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 }
